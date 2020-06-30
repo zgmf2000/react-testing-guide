5fc12b7a49497945c66ed0a06de963e79f5eefb3
Bintoro Adi Guna <saviour.of.darkness@gmail.com>
2020-06-30 09:05:17 +0700

    Refactored Folder Structures

---
+++ src/pages/home/Home.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Home from './Home';
+
+describe('Home', () => {
+test('renders learn react link', () => {
+    const { getByText } = render(<Home />);
+    const linkElement = getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+})
